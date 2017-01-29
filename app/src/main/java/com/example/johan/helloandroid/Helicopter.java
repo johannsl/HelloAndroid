7be72d9b0ebe95298d7183f8ee7ed92b2b0930d5
johannsl <johannsl@stud.ntnu.no>
2017-01-29 16:51:35 +0100

    helicopter now moves after screen touch

--- app/src/main/java/com/example/johan/helloandroid/Helicopter.java
+++ app/src/main/java/com/example/johan/helloandroid/Helicopter.java
@@ -9,32 +9,55 @@ import sheep.graphics.Image;
 
 public class Helicopter extends Sprite {
 
-    private final int VELOCITY = 100;
-    private float[] coordinates;
+    private final int VELOCITY = 1000;
     private float[] vector;
 
     public Helicopter(Image image) {
         super(image);
         setPosition(1000, 1000);
-        coordinates = new float[] {1000f, 1000f};
         vector = new float[] {1f, 0f};
     }
 
     @Override
     public void update(float dt) {
         super.update(dt);
-        
+        checkWallCollision();
+        setImageDirection();
+        moveHelicopter(dt);
+    }
 
+    private void setImageDirection() {
+        if (this.vector[0] >= 0) {
+            setScale(-1, 1);
+        }
+        else { setScale(1, 1); }
+    }
+
+    private void checkWallCollision() {
+        if (getX() >= MyGame.width && vector[0] >= 0) {
+            vector[0] = -vector[0];
+        }
+        else if (getX() <= 0 && vector[0] <= 0) {
+            vector[0] = -vector[0];
+        }
+        if (getY() >= MyGame.height && vector[1] >= 0) {
+            vector[1] = -vector[1];
+        }
+        else if (getY() <= 0 && vector[1] <= 0) {
+            vector[1] = -vector[1];
+        }
+    }
+
+    private void moveHelicopter(float dt) {
+        float dx = dt * VELOCITY * vector[0];
+        float dy = dt * VELOCITY * vector[1];
+        setPosition(getX() + dx, getY() + dy);
     }
 
     public void setVector(float[] coordinates) {
-        this.vector[0] = coordinates[0] - this.coordinates[0];
-        this.vector[1] = coordinates[1] - this.coordinates[1];
+        this.vector[0] = coordinates[0] - getX();
+        this.vector[1] = coordinates[1] - getY();
         this.vector = normalize(this.vector);
-
-        for (float element : this.vector){
-            System.out.println("VECTOR ###### " + element);
-        }
     }
 
     private float[] normalize(float[] vector) {
